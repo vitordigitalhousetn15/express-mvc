bd10d512298362e98a0950194a0613334fd26948
Vitor Costa <vitor.costa@prontmed.com>
2021-01-14 18:00:03 -0300

    add: products route

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const products = require('./model/products.json')
+
+const PORT = 3000
+
+const app = express()
+
+// req = request ou requisição
+// res = response ou resposta
+app.get('/', function(req, res) {
+    res.send('hello world')
+})
+// metodo: get, post, put, delete, patch
+app.get('/products', function(req, res) {
+    // http://localhost:3000/products?type=TV
+    const type = req.query.type
+    if(type === undefined) {
+        res.json({
+            data: products
+        })
+    } else {
+        const data = products.filter(function(product) {
+            return product.type === type
+        })
+        res.json({
+            data: data
+        })
+    }
+})
+
+app.listen(PORT, function() {
+    console.log("rodando servidor")
+})
